06c7d2b6555a116112d2a2c46cb143f16de4132b
Charly Rosetto <charly.rosetto@ynov.com>
2025-09-08 16:12:50 +0200

    checknumber

---
+++ checknumber.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func CheckNumber(arg string) bool {
+	for _, r := range arg {
+		if (r < 'a' || r > 'z') && (r < 'A' || r > 'Z' ) {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	fmt.Println(CheckNumber("Hello"))
+	fmt.Println(CheckNumber("Hello1"))
+}
